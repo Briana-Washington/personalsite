e68a82f54c8743dcc0a41b6bb531d32e43a01b41
briana <bwashington1907@gmail.com>
2018-08-20 05:08:28 +0000

    add all files

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,14 +1,6 @@
-//Identify the button
 var navToggle = document.getElementById('nav-toggle');
-//Identify the nav items
-var navItems = document.getElementById('nav-items')
-//Button is waiting to be clicked
+var navItems = document.getElementById('nav-items');
+
 navToggle.addEventListener('click', function(){
-  
-  //Button gets clicked
-  //Nav items disappear
-  
-  //Button gets clicked again
-  //Nav items disappear
   navItems.classList.toggle('mobile-nav-hidden');
 });
